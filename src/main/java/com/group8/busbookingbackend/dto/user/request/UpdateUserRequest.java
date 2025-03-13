a3caddf4d603fa0c1f40933b426ab374ccedf136
NgHoaiTan <tannh3108@gmail.com>
2025-03-13 07:59:10 +0700

    add DTO UpdateUserRequest

---
+++ src/main/java/com/group8/busbookingbackend/dto/user/request/UpdateUserRequest.java
@@ -0,0 +1,14 @@
+package com.group8.busbookingbackend.dto.user.request;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class UpdateUserRequest {
+    private String name;
+    private LocalDateTime dateOfBirth;
+    private String gender;
+    private String phoneNumber;
+    private String avatarUrl;
+}
